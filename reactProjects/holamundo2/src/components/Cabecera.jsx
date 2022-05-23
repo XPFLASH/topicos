1e2f0c14ecc7c6d5b0e8ec8a3c6f6b4c8c2cd515
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2022-05-23 12:34:10 -0700

    a

--- reactProjects/holamundo2/src/components/Cabecera.jsx
+++ reactProjects/holamundo2/src/components/Cabecera.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Cabecera = () => {
     return (
-        <h1 className="text-[30px] font-bold uppercase bg-[#B6BDDE] border-4 border-solid border-black text-center p-10 m-[40px] rounded-2xl shadow-xl break-all">Administrador de Citas</h1>
+        <h1 className="text-[30px] font-bold uppercase bg-[#B6BDDE] border-4 border-solid border-black text-center p-7 m-7 rounded-2xl shadow-xl break-all">Administrador de Citas</h1>
     )
 }
--- reactProjects/holamundo2/src/components/Listado.jsx
+++ reactProjects/holamundo2/src/components/Listado.jsx
@@ -5,7 +5,7 @@ import { Paciente } from './Paciente'
 export const Listado = ({ pacientes, setPaciente, eliminarPaciente }) => {
 
     return (
-        <div className="bg-[#B6BDDE] my-8 mx-8 p-8 border-4 border-solid border-black rounded-md md:w-1/2 lg:w-3/5 md:14px overflow-y-scroll md:h-screen">
+        <div className="bg-[#B6BDDE] my-7 mx-7 p-7 border-4 border-solid border-black rounded-md md:w-1/2 lg:w-3/5 md:14px overflow-y-scroll md:h-screen">
 
             <h2 className='text-center font-bold text-2xl'>Listado Paciente</h2>
             <p className='mt-5 text-xl'>Administra tus <span className='font-bold'>Pacientes y Citas</span></p>
