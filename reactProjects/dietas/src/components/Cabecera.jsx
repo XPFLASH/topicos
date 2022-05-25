383ffb12b28be6a412b4406e58a8a3ce8337a219
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2022-05-25 11:35:26 -0700

    aa

--- reactProjects/dietas/src/components/Cabecera.jsx
+++ reactProjects/dietas/src/components/Cabecera.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Cabecera = () => {
     return (
-        <h1 className="text-[30px] font-bold uppercase bg-[#B6BDDE] border-4 border-solid border-black text-center p-6 m-6 rounded-2xl shadow-xl break-all">Administrador de Citas</h1>
+        <h1 className="text-[30px] font-bold uppercase bg-[#B6BDDE] border-4 border-solid border-black text-center p-6 m-6 rounded-2xl shadow-xl break-all">Control de Peso</h1>
     )
 }
