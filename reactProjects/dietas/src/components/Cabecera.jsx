d7a368d59da7a11ce3457ec07cea9264180087b3
RICARDO ESCOBAR CESEÑA <95449905+XPFLASH@users.noreply.github.com>
2022-05-25 11:40:07 -0700

    cambio

--- reactProjects/dietas/src/components/Cabecera.jsx
+++ reactProjects/dietas/src/components/Cabecera.jsx
@@ -5,3 +5,6 @@ export const Cabecera = () => {
         <h1 className="text-[30px] font-bold uppercase bg-[#B6BDDE] border-4 border-solid border-black text-center p-6 m-6 rounded-2xl shadow-xl break-all">Control de Peso</h1>
     )
 }
+//Nombre
+//Genero
+//Peso, estatura, edad
